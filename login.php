bc2074f622e9b602dd91749b23a9bee599bb8703
gfftr <gff.terl@gmail.com>
2023-02-28 22:31:07 +0000

    Update login

--- login.php
+++ login.php
@@ -1,3 +1,27 @@
+<?php
+
+require "functions.php";
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+ $email = addslashes($_POST['email']);
+ $password = addslashes($_POST['password']);
+
+ $query = "SELECT * FROM users WHERE email = '$email' && password = '$password' limit 1";
+
+ $result = mysqli_query($con, $query);
+
+ //print_r(mysqli_num_rows($result) > 0);
+ if (mysqli_num_rows($result)) {
+  $row = mysqli_fetch_assoc(); // fetches array change to associate array
+  header("Location: profile.php");
+  die;
+ } else {
+  // error
+  $error = "Wrong email or password";
+ }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,8 +42,8 @@
    <div>
     <h3>Login</h3>
     <form method="post">
-     <input type="text" name="email" placeholder="Email"><br>
-     <input type="text" name="password" placeholder="Password"><br>
+     <input type="text" name="email" placeholder="Email" require><br>
+     <input type="text" name="password" placeholder="Password" require><br>
      <button>Signup</button>
     </form>
    </div>
